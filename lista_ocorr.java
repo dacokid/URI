ebe30775dbda84064d00b65be8bce8992700fc8b
Daniel Cunha <danielcc.jp@gmail.com>
2019-12-04 14:39:00 -0300

    Update lista_ocorr.java

--- lista_ocorr.java
+++ lista_ocorr.java
@@ -1,40 +1,41 @@
 import java.util.Scanner;
 
-
 public class lista_ocorr {
 	
-	public static void main(String[] args) {
-		int n;
-		int m;
-		int tam = 0;
-		Scanner entrada = new Scanner(System.in);
-		n = entrada.nextInt();
-		m = entrada.nextInt();
-		int dados[] = new int[n];
-		for(int i = 0; i < n; i++) {
-			int valor = entrada.nextInt();
-			dados[tam] = valor;
-			tam++;
-		}
+    public static void main(String[] args) {
+	int n;
+	int m;
+	Scanner entrada = new Scanner(System.in);
+        do{
+            int tam = 0;
+            n = entrada.nextInt();
+            m = entrada.nextInt();
+            int dados[] = new int[n];
+            for(int i = 0; i < n; i++) {
+                int valor = entrada.nextInt();
+                dados[tam] = valor;
+                tam++;
+            }
 		
-		for(int i = 0; i < m; i++) {
-			int aux = 0;
-			int indice = 0;
-			int ocor = entrada.nextInt();
-			int val = entrada.nextInt();
-			for(int x = 0; x < n; x++) {
-				if(dados[x] == val) {
-					aux++;
-					if(aux == ocor) {
-						indice = x+1;
-						break;
-					} 
-				}
-			}
-			
-			if(aux != ocor) {
-				System.out.println("0");
-			} else System.out.println(indice);
+            for(int i = 0; i < m; i++) {
+		int aux = 0;
+		int indice = 0;
+		int k = entrada.nextInt(); // ocorr
+		int v = entrada.nextInt(); //valor
+		for(int x = 0; x < n; x++) {
+                    if(dados[x] == v) {
+                        aux++;
+                        if(aux == k) {
+                            indice = x+1;
+                            break;
+			} 
+                    }
 		}
-	}
+			
+		if(aux != k) {
+                    System.out.println("0");
+		} else System.out.println(indice);
+            }
+        } while(entrada.hasNextLine());
+    }
 }
